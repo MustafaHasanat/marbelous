8e66e0cc83c77974442af6273fb45936d0d1decf
MustafaHasanat <mustafa.hasanat99@gmail.com>
2025-01-15 15:11:50 +0300

    feat: adding features

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,104 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { jwtGuard } from "../../actions";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(
+    request: NextRequest,
+    { params: { id } }: { params: { id: string } }
+) {
+    try {
+        await jwtGuard(request);
+
+        const user = await prisma.user.findUnique({
+            where: { id },
+        });
+
+        return NextResponse.json(
+            {
+                data: user,
+                error: null,
+                status: true,
+            },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                data: null,
+                error: error.message || "An unexpected error occurred.",
+                status: false,
+            },
+            { status: 500 }
+        );
+    }
+}
+
+export async function PATCH(
+    request: NextRequest,
+    { params: { id } }: { params: { id: string } }
+) {
+    try {
+        await jwtGuard(request);
+
+        const formData: FormData = await request.formData();
+
+        const user = await prisma.user.update({
+            where: { id },
+            data: {
+                name: formData.get("name") as string,
+            },
+        });
+
+        return NextResponse.json(
+            {
+                data: user,
+                error: null,
+                status: true,
+            },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                data: null,
+                error: error.message || "An unexpected error occurred.",
+                status: false,
+            },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(
+    request: NextRequest,
+    { params: { id } }: { params: { id: string } }
+) {
+    try {
+        await jwtGuard(request);
+
+        const user = await prisma.user.delete({
+            where: { id },
+        });
+
+        return NextResponse.json(
+            {
+                data: user,
+                error: null,
+                status: true,
+            },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                data: [],
+                error: error.message || "An unexpected error occurred.",
+                status: false,
+            },
+            { status: 500 }
+        );
+    }
+}
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { jwtGuard } from "../actions";
 
 export const dynamic = "force-dynamic";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     try {
+        await jwtGuard(request);
+
         const users = await prisma.user.findMany();
 
         return Response.json({
@@ -17,37 +20,6 @@ export async function GET() {
         return NextResponse.json(
             {
                 data: [],
-                error: error.message || "An unexpected error occurred.",
-                status: false,
-            },
-            { status: 500 }
-        );
-    }
-}
-
-export async function POST(request: NextRequest) {
-    try {
-        const formData: FormData = await request.formData();
-
-        const user = await prisma.user.create({
-            data: {
-                name: formData.get("name") as string,
-                email: formData.get("email") as string,
-            },
-        });
-
-        return NextResponse.json(
-            {
-                data: user,
-                error: null,
-                status: true,
-            },
-            { status: 201 }
-        );
-    } catch (error: any) {
-        return NextResponse.json(
-            {
-                data: null,
                 error: error.message || "An unexpected error occurred.",
                 status: false,
             },
