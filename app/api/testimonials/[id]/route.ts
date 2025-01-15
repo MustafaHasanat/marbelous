056afc21352d2e162a2afcd81a930bfa50667a65
MustafaHasanat <mustafa.hasanat99@gmail.com>
2025-01-15 15:07:13 +0300

    feat: adding features

---
+++ app/api/testimonials/[id]/route.ts
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { jwtGuard } from "../../actions";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(
+    request: NextRequest,
+    { params: { id } }: { params: { id: string } }
+) {
+    try {
+        await jwtGuard(request);
+
+        const testimonial = await prisma.testimonial.findUnique({
+            where: { id: id },
+        });
+
+        return NextResponse.json(
+            {
+                data: testimonial,
+                error: null,
+                status: true,
+            },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                data: [],
+                error: error.message || "An unexpected error occurred.",
+                status: false,
+            },
+            { status: 500 }
+        );
+    }
+}
+
+export async function PATCH(
+    request: NextRequest,
+    { params: { id } }: { params: { id: string } }
+) {
+    try {
+        await jwtGuard(request);
+
+        const formData: FormData = await request.formData();
+        const image = formData.get("image") as string;
+
+        const testimonial = await prisma.testimonial.update({
+            where: { id },
+            data: {
+                name: formData.get("name") as string,
+                message: formData.get("message") as string,
+                ...(image ? { image } : {}),
+            },
+        });
+
+        return NextResponse.json(
+            {
+                data: testimonial,
+                error: null,
+                status: true,
+            },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                data: null,
+                error: error.message || "An unexpected error occurred.",
+                status: false,
+            },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(
+    request: NextRequest,
+    { params: { id } }: { params: { id: string } }
+) {
+    try {
+        await jwtGuard(request);
+
+        const testimonial = await prisma.testimonial.delete({
+            where: { id: id },
+        });
+
+        return NextResponse.json(
+            {
+                data: testimonial,
+                error: null,
+                status: true,
+            },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                data: [],
+                error: error.message || "An unexpected error occurred.",
+                status: false,
+            },
+            { status: 500 }
+        );
+    }
+}
--- prisma/migrations/20250115120658_init/migration.sql
+++ prisma/migrations/20250115120658_init/migration.sql
@@ -39,11 +39,37 @@ CREATE TABLE "Order" (
     CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Testimonial" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT,
+    "message" TEXT,
+
+    CONSTRAINT "Testimonial_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Marbelous" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "data" JSONB NOT NULL,
+
+    CONSTRAINT "Marbelous_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Order_itemId_key" ON "Order"("itemId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Marbelous_identifier_key" ON "Marbelous"("identifier");
 
 -- AddForeignKey
 ALTER TABLE "Order" ADD CONSTRAINT "Order_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item"("id") ON DELETE SET NULL ON UPDATE CASCADE;
