6534fadb97b99a38af1906df70db4329ea7dd3b2
MustafaHasanat <mustafa.hasanat99@gmail.com>
2025-01-15 14:35:06 +0300

    feat: adding features

---
+++ lib/aws.ts
@@ -0,0 +1,20 @@
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
+export const uploadToS3 = async (file: File) => {
+    const params = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME || "",
+        Key: `${Date.now()}-${file.name}`,
+        Body: file.arrayBuffer,
+        ContentType: file.type,
+        ACL: "public-read",
+    };
+
+    const result = await s3.upload(params).promise();
+    return result.Location; // URL of the uploaded file
+};
